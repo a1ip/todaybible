bcefdf4db0b6b64c640507b8565ea369fb9a6af1
Phil Rigovanov <phil@rigovanov.ru>
2014-05-21 12:09:49 +0400

    book names case added

---
+++ todaybible.rb
@@ -0,0 +1,167 @@
+require 'nokogiri'
+
+@testament = 1
+@book = 1
+@chapter =1
+
+if ARGV
+	@book, @chapter = ARGV[0], ARGV[1]
+	# @testament = 2 if ARGV[0].to_i > 39
+end
+
+
+def booknumber (bookname)
+	case bookname
+	when "Быт."
+		1
+	when "Исх."
+		2
+	when "Лев."
+		3
+	when "Чис."
+		4
+	when "Втор."
+		5
+	when "Ис.Нав."
+		6
+	when "Суд."
+		7
+	when "Руфь"
+		8
+	when "1Цар."
+		9
+	when "2Цар."
+		10
+	when "3Цар."
+		11
+	when "4Цар."
+		12
+	when "1Пар."
+		13
+	when "2Пар."
+		14
+	when "Езд."
+		15
+	when "Неем."
+		16
+	when "Есф."
+		17
+	when "Иов"
+		18
+	when "Пс."
+		19
+	when "Пр."
+		20
+	when "Еккл."
+		21
+	when "П.Песней"
+		22
+	when "Ис."
+		23
+	when "Иерем."
+		24
+	when "Пл.Иер."
+		25
+	when "Иез."
+		26
+	when "Дан."
+		27
+	when "Осия"
+		28
+	when "Иоиль"
+		29
+	when "Амос"
+		30
+	when "Авдий"
+		31
+	when "Иона"
+		32
+	when "Михея"
+		33
+	when "Наума"
+		34
+	when "Аввакум"
+		35
+	when "Софония"
+		36
+	when "Аггей"
+		37
+	when "Зах."
+		38
+	when "Малахия"
+		39
+	when "Мтф."
+		40
+	when "Марк."
+		41
+	when "Лук."
+		42
+	when "Иоан."
+		43
+	when "Деян."
+		44
+	when "Иакова"
+		45
+	when "1Петр."
+		46
+	when "2Петр."
+		47
+	when "1Иоан."
+		48
+	when "2Иоан."
+		49
+	when "3Иоан."
+		50
+	when "Иуды"
+		51
+	when "Рим."
+		52
+	when "1Кор."
+		53
+	when "2Кор."
+		54
+	when "Гал."
+		55
+	when "Ефесянам"
+		56
+	when "Филип."
+		57
+	when "Колос."
+		58
+	when "1Фес."
+		59
+	when "2Фес."
+		60
+	when "1Тим."
+		61
+	when "2Тим."
+		62
+	when "Титу"
+		63
+	when "Филимону"
+		64
+	when "Евр."
+		65
+	when "Откр."
+		66
+	else
+		
+	end
+	
+end
+
+def printplace
+	f = File.open("Bible_par.fb2")
+	doc = Nokogiri::XML(f)
+	# body:nth-of-type(#{@testament})
+	doc.css("body section:nth-of-type(#{@book}) section:nth-of-type(#{@chapter}) p").each do |paragraph|
+		paragraph.css("sup").each do |verse|
+			print verse.next_sibling.content.gsub(/^\s/, "")		
+		end
+		puts
+	end
+	f.close
+end
+
+printplace
+
